b38b2a5201f54c7f77978304404ef83c691a1b69
tomjohnburton <tomjohnburton@users.noreply.github.com>
2018-11-10 15:59:57 +0100

    Nav changes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+      
