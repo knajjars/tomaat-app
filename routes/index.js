efa088c0caab6e895f89bc2ad0d1d577802f4365
tomjohnburton <tomjohnburton@users.noreply.github.com>
2018-11-11 16:40:54 +0100

    LP changes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router  = express.Router();
+const User = require("../models/User");
 
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+User.find()
+
 
 module.exports = router;
