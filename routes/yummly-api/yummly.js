064f93884439d0171bd107eabebe04612ee74a7c
Khalil Najjar <knajjars@gmail.com>
2018-11-12 10:40:08 +0100

    yummly api starter code

--- routes/yummly-api/yummly.js
+++ routes/yummly-api/yummly.js
@@ -7,4 +7,18 @@ Yummly.config({
   app_key: process.env.API_KEY
 });
 
+router.post("/something-new", (req, res, next) => {
+  Yummly.query("pineapple")
+    .maxTotalTimeInSeconds(1400)
+    .maxResults(20)
+    .allowedDiets(["Pescetarian", "Vegan"])
+    .allowedCuisines(["asian"])
+    .minRating(3)
+    .get()
+    .then(function(resp) {
+      resp.matches.forEach(function(recipe) {
+        console.log(recipe.recipeName);
+      });
+    });
+});
 module.exports = router;
