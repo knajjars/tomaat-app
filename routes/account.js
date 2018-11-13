fd3afbc003d9b6cd48776772d155c90af5ac26ac
Khalil Najjar <knajjars@gmail.com>
2018-11-13 09:25:27 +0100

    morning sync

---
+++ routes/account.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+const User = require("../models/User");
+const ensureAuthenticated = require("./Secuirty/ensureAuthenticated");
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+module.exports = router;
