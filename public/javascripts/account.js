74efa1859fd497df27459d2a6e695b1791f8addd
Khalil Najjar <knajjars@gmail.com>
2018-11-14 12:28:04 +0100

    account css almost done

--- public/javascripts/account.js
+++ public/javascripts/account.js
@@ -2,4 +2,21 @@ window.onload = function() {
   axios.get("/account/user-preferences").then(response => {
     console.log(response.data);
   });
+
+  $(".tabs").tabs({ swipeable: true });
+  $(".carousel").append(`
+  <div class="carousel-fixed-item center">
+  <a class="btn waves-effect white grey-text darken-text-2 button-save">
+  <i class="material-icons">cloud_done</i>
+  Save</a>
+</div>
+  `);
+  $(".carousel.carousel-slider").carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
+  $(document).ready(function() {
+    $(".collapsible").collapsible();
+  });
 };
